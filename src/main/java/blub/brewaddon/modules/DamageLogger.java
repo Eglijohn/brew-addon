e20a94d500f0469abccf4f6044384322b9e430ea
Eglijohn <eglijohn@posteo.ch>
2025-09-27 21:12:15 +0200

    Update

--- src/main/java/blub/brewaddon/modules/DamageLogger.java
+++ src/main/java/blub/brewaddon/modules/DamageLogger.java
@@ -20,13 +20,6 @@ import static blub.brewaddon.utils.misc.TextUtils.formatMinecraftString;
 public class DamageLogger extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Boolean> enableGlobalMode = sgGeneral.add(new BoolSetting.Builder()
-        .name("enable-global-mode")
-        .description("Enable global health logging (for simpcraft.com)")
-        .defaultValue(false)
-        .build()
-    );
-
     private final Setting<logMode> logModeSetting = sgGeneral.add(new EnumSetting.Builder<logMode>()
         .name("log-mode")
         .description("How to log the damage events")
@@ -39,82 +32,14 @@ public class DamageLogger extends Module {
         .defaultValue(attackMode.FriendsAndSelf)
         .build());
 
-    private final Setting<Boolean> ignoreList = sgGeneral.add(new BoolSetting.Builder()
-        .name("ignoreList")
-        .description("Ignores List")
-        .defaultValue(false)
-        .visible(enableGlobalMode::get)
-        .build()
-    );
-
-    private final Setting<List<String>> players = sgGeneral.add(new StringListSetting.Builder()
-        .name("players")
-        .description("Players where the health is logged")
-        .defaultValue(List.of("Sentoljaard"))
-        .visible(() -> enableGlobalMode.get() && !ignoreList.get())
-        .build()
-    );
-
-    private final Setting<Boolean> useRegex = sgGeneral.add(new BoolSetting.Builder()
-        .name("regex")
-        .description("Pain to write")
-        .defaultValue(false)
-        .visible(enableGlobalMode::get)
-        .build()
-    );
-
-    private final Setting<String> regex = sgGeneral.add(new StringSetting.Builder()
-        .name("players")
-        .description("Players where the health is logged")
-        .defaultValue("")
-        .visible(() -> enableGlobalMode.get() && useRegex.get())
-        .build()
-    );
-
-    private final Setting<Boolean> cantReadNumbers = sgGeneral.add(new BoolSetting.Builder()
-        .name("no numbers")
-        .description("if you don't know that -> 5")
-        .defaultValue(true)
-        .visible(enableGlobalMode::get)
-        .build()
-    );
-
     public DamageLogger() {
         super(BrewAddon.CATEGORY, "b-damage-logger", "Logs damage events");
     }
 
     @EventHandler
     private void onReceivePacket(PacketEvent.Receive event) {
-        if (!(event.packet instanceof EntityDamageS2CPacket packet)) {
-            if (enableGlobalMode.get()) {
-                Packet packet2 = event.packet;
-                if (packet2 instanceof ScoreboardScoreUpdateS2CPacket) {
-                    ScoreboardScoreUpdateS2CPacket scoreboardPacket = (ScoreboardScoreUpdateS2CPacket) packet2;
-                    if (scoreboardPacket.objectiveName().contains("TAB-BelowName")) {
-                        String name = scoreboardPacket.scoreHolderName();
-                        int health = scoreboardPacket.score();
-                        Pattern pattern = Pattern.compile(regex.get(), Pattern.CASE_INSENSITIVE);
-                        if (players.get().contains(name) || ignoreList.get() || (useRegex.get() && pattern.matcher(name).find())) {
-                            if (!cantReadNumbers.get()) {
-                                info("Player " + name + " has " + health + " health");
-                            } else {
-                                StringBuilder hpstring = new StringBuilder();
-                                int fullHearts = health / 2;  // Number of full hearts
-                                boolean hasHalfHeart = health % 2 != 0; // Check if there's a half heart
-                                for (int i = 0; i < fullHearts; i++) {
-                                    hpstring.append("❤");
-                                }
-                                if (hasHalfHeart) {
-                                    hpstring.append("♡"); // Half heart
-                                }
-                                info(name + ": " + hpstring);
-                            }
-                        }
-                    }
-                }
-            }
-            return;
-        }
+        if (!(event.packet instanceof EntityDamageS2CPacket packet)) return;
+
 
         Entity attacker = mc.world.getEntityById(packet.sourceCauseId());
         Entity target = mc.world.getEntityById(packet.entityId());
